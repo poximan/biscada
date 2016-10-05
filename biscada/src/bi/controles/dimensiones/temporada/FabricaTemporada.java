e9bfb9064254ce9ac6bae816dd3ad33c3746df83
hdonato <hdonato@pc0137.servicoop.com>
2016-10-05 08:48:53 -0300

    agregando plantilla comentario

--- biscada/src/bi/controles/dimensiones/temporada/FabricaTemporada.java
+++ biscada/src/bi/controles/dimensiones/temporada/FabricaTemporada.java
@@ -13,6 +13,26 @@ import comunes.modelo.Alarma;
 /* CLASE ....................................... */
 /* ............................................. */
 
+/**
+ * Class Responsibility Collaborator (CRC) design:
+ * 
+ * ==== parte clase =========================
+ * 
+ * YO REPRESENTO,
+ * 
+ * ==== parte responsabilidad ===============
+ * 
+ * LO QUE HAGO,
+ * 
+ * LO QUE CONOZCO,
+ * 
+ * ==== parte colaboracion ==================
+ * 
+ * MI COLABORADOR PRINCIPAL,
+ * 
+ * COMO INTERACTUO CON MI COLABORADOR,
+ *
+ */
 public class FabricaTemporada {
 
 	/* ............................................. */
